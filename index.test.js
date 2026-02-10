adbaa5fb2ecdd796c22903bfeb582484b0f837a3
Hara.h <hara.h+sourcetree@ict4e.jp>
2026-02-10 17:29:30 +0900

    Add multiply function

--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
-const {sum} = require('./index');
+const {sum, multiply} = require('./index');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('multiplies 2 * 3 to equal 6', () => {
+    expect(multiply(2, 3)).toBe(6);
+}); 
